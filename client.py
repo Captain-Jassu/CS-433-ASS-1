cd5b65acf23ab3afee0b0b442a609d8e6a4f4b88
Jayasurya Nalamolu <88476397+Captain-Jassu@users.noreply.github.com>
2022-09-16 17:40:50 +0530

    Update client.py

--- client.py
+++ client.py
@@ -7,8 +7,17 @@ def encrypt(str, mode):
     if(mode=='1'):
         new_str = str
     elif(mode=='2'):
+        offset=2
         for i in str:
-            new_str+= chr(ord(i)+2)
+            if(48<=ord(i) and ord(i)<=57):
+                new_str += chr((ord(i)+offset-48)%10 + 48)
+            elif(65<=ord(i) and ord(i)<=90):
+                new_str += chr((ord(i)+offset-65)%26 + 65)
+            elif(97<=ord(i) and ord(i)<=122):
+                new_str += chr((ord(i)+offset-97)%26 + 97)
+            else:
+                new_str+=i
+    
     elif(mode=='3'):
         list = str.split(" ")
         for i in list:
@@ -22,8 +31,16 @@ def decrypt(str, mode):
     if(mode=='1'):
         new_str = str
     elif(mode=='2'):
+        offset=2
         for i in str:
-            new_str+= chr(ord(i)-2)
+            if(48<=ord(i) and ord(i)<=57):
+                new_str += chr((ord(i)-offset-48+10)%10 + 48)
+            elif(65<=ord(i) and ord(i)<=90):
+                new_str += chr((ord(i)-offset-65+26)%26 + 65)
+            elif(97<=ord(i) and ord(i)<=122):
+                new_str += chr((ord(i)-offset-97+26)%26 + 97)
+            else:
+                new_str+=i
     elif(mode=='3'):
         list = str.split(" ")
         for i in list:
@@ -59,29 +76,20 @@ while True:
         path = decrypt(en_path,mode)
         print(path)
     
-    if(args[0]=="ls"):
+    elif(args[0]=="ls"):
         en_str = client.recv(SIZE).decode()
         str = decrypt(en_str,mode)
         list_dir = str.split(",")
         print(list_dir)
     
-    if(args[0]=="cd"):
+    elif(args[0]=="cd"):
         en_dir = client.recv(SIZE).decode()
         curr_dir = decrypt(en_dir,mode)
         if(curr_dir=="NOK"):
             print("NOK")
         else:
+            print("OK")
             print(curr_dir)
-
-    if(args[0]=='upd'):
-        file_path = args[1]
-
-        # file path sending
-        file = open(file_path,'r')
-        data = file.read()
-        en_data = encrypt(data,mode)
-        file.close()
-        client.send(en_data.encode())
 
     elif(args[0]=='dwd'):
         file_path = args[1]
@@ -91,8 +99,28 @@ while True:
         en_data = client.recv(SIZE).decode()
         data = decrypt(en_data,mode)
 
-        new_file = open(file_name,'w')
-        new_file.write(data)
-        new_file.close()
+        if(data=="NOK"):
+            print("NOK")
+        else:
+            new_file = open(file_name,'w')
+            new_file.write(data)
+            new_file.close()
+            print("OK")
+
+    elif(args[0]=='upd'):
+        file_path = args[1]
+
+        try:
+        # sending the data from file by reading it. 
+            file = open(file_path,'r')
+            data = file.read()
+            en_data = encrypt(data,mode)
+            file.close()
+            client.send(en_data.encode())
+            print("OK")
+        except:
+            en_status = encrypt("NOK",mode)
+            client.send(en_status.encode())
+            print("NOK")
 
 client.close()
